a9fe03d3c819697e1fb77248d7a494fc241ff6a9
jeevanpuhan <jeevankumarpuhan@gmail.com>
2023-02-02 11:16:21 +0530

    Add Domain models and integrate EF Core

---
+++ BankingBrokerage.API/Data/BankingBrokerageDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BankingBrokerage.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankingBrokerage.API.Data
+{
+    public class BankingBrokerageDbContext : DbContext
+    {
+        public BankingBrokerageDbContext(DbContextOptions<BankingBrokerageDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
